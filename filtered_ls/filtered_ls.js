eea74197cd324501fa43bc2f167c28182179903a
Chris Bynum <chris.bynum@levvel.io>
2017-07-12 14:37:30 -0400

    Completed filtered ls assignment.

---
+++ filtered_ls/filtered_ls.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+var path = require('path')
+
+fs.readdir(process.argv[2], function getFiles(err, files) {
+  if(err) console.error(err);
+  var inputExt = process.argv[3].toString();
+  files.forEach(function(value){
+    if(path.extname(value).replace('.','') === inputExt) console.log(value);
+  });
+});
