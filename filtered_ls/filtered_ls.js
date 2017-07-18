7be1d2f149b6bfc42e1f4ae045e9aa84319ff619
Chris Bynum <chris.bynum@levvel.io>
2017-07-17 23:01:01 -0400

    Removed semicolons

--- filtered_ls/filtered_ls.js
+++ filtered_ls/filtered_ls.js
@@ -2,9 +2,9 @@ var fs = require('fs')
 var path = require('path')
 
 fs.readdir(process.argv[2], function getFiles(err, files) {
-  if(err) console.error(err);
-  var inputExt = process.argv[3].toString();
+  if(err) console.error(err)
+  var inputExt = process.argv[3].toString()
   files.forEach(function(value){
-    if(path.extname(value).replace('.','') === inputExt) console.log(value);
-  });
-});
+    if(path.extname(value).replace('.','') === inputExt) console.log(value)
+  })
+})
